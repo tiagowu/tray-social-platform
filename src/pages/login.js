a4f712b007e95ecd8a5025bc90c117b12a936660
Tiago <tiagowu12@gmail.com>
2023-01-01 22:08:46 -0500

    reformatted code

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,17 +1,19 @@
-import React from 'react';
-  
+import React from "react";
+
 const Login = () => {
   return (
     <div>
       <form>
-          <label for="user">Username:</label>
-          <input type="text" id="user" name ="user"/><br></br>
-          <label for="password">Password:</label>
-          <input type="text" id="password" name = "password"/><br></br>
-          <input type = "submit" value = "Submit"></input>
-        </form>
+        <label for="user">Username:</label>
+        <input type="text" id="user" name="user" />
+        <br></br>
+        <label for="password">Password:</label>
+        <input type="text" id="password" name="password" />
+        <br></br>
+        <input type="submit" value="Submit"></input>
+      </form>
     </div>
   );
 };
-  
+
 export default Login;
