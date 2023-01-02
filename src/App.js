d42391bceeddc47ca0d24c070ddd0c45ad04ae4a
Tiago <tiagowu12@gmail.com>
2023-01-01 22:05:57 -0500

    reformatted code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import SignUp from './pages/signup';
-import Login from './pages/login'
-import { BrowserRouter as Router, Routes, Route}
-    from 'react-router-dom';
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-        <Router>
-          <Routes>
-            <Route path="/" element={<Login/>} />
-            <Route path='/signup' element={<SignUp/>} />
-          </Routes>
-        </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 }
 
