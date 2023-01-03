4bf0098482417f32e70e753aae8d2fdd8c155a97
Tiago <tiagowu12@gmail.com>
2023-01-02 22:04:46 -0500

    added header and login link

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import SignUpForm from "../components/SignUpForm";
+import "./SignUp.css";
 
 const SignUp = () => {
   return (
-    <div>
-      <SignUpForm />
+    <div className="signup">
+      <div className="signup-container">
+        <h1>Tray</h1>
+        <SignUpForm />
+        <p>
+          Already have an account?{" "}
+          <Link to="/" className="login-link">
+            Log in
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
