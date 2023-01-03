f749759e7f61344ef9fd2cc06daecdf5102fb2a8
Tiago <tiagowu12@gmail.com>
2023-01-02 22:01:12 -0500

    added value and handlechange prop

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,13 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import "./TextInput.css";
 
-const TextInput = ({ type = "text", label }) => {
-  const [value, setValue] = useState("");
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
-
+const TextInput = ({ type, label, value, handleChange }) => {
   return (
     <div className="input-container">
       <input type={type} value={value} onChange={handleChange} />
