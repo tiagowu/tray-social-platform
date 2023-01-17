363c58f77c42776abf258fd59107618805c1d9c6
Tiago <tiagowu12@gmail.com>
2023-01-16 22:51:55 -0500

    added name as prop to handle onChange

--- frontend/src/components/TextInput.jsx
+++ frontend/src/components/TextInput.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./TextInput.css";
 
-const TextInput = ({ type, label, value, handleChange }) => {
+const TextInput = ({ type, label, name, value, handleChange }) => {
   return (
     <div className="input-container">
-      <input type={type} value={value} autoComplete="off" onChange={handleChange} />
+      <input type={type} value={value} name={name} autoComplete="off" onChange={handleChange} />
       <label className={value && "filled"}>{label}</label>
     </div>
   );
