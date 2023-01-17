d74ceb4680c6d98d4bae1281e08af75adf5cd32b
Tiago <tiagowu12@gmail.com>
2023-01-17 15:21:21 -0500

    added loading display on button and changed code formatting

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 import TextInput from "./TextInput";
 import "./LoginForm.css";
 import { login } from "../redux/actions/authActions";
+import { clearAlert } from "../redux/actions/alertActions";
 import { useDispatch, useSelector } from "react-redux";
 
 const LoginForm = () => {
   const { alert } = useSelector((state) => state);
-  const initialState = { email: localStorage.getItem("email"), password: "" };
+  const initialState = { email: "", password: "" };
   const [userData, setUserData] = useState(initialState);
   const [showPass, setShowPass] = useState(false);
   const { email, password } = userData;
@@ -21,38 +22,21 @@ const LoginForm = () => {
   const handleDataChange = (e) => {
     const { name, value } = e.target;
     setUserData({ ...userData, [name]: value });
-    localStorage.setItem("email", email);
   };
 
   return (
     <form className="login-form" onSubmit={handleSubmit}>
-      <TextInput
-        type="text"
-        label="Email"
-        name="email"
-        value={email}
-        handleChange={handleDataChange}
-      />
+      <TextInput type="text" label="Email" name="email" value={email} handleChange={handleDataChange} />
       <div className="password-input">
-        <TextInput
-          type={showPass ? "text" : "password"}
-          label="Password"
-          name="password"
-          value={password}
-          handleChange={handleDataChange}
-        />
+        <TextInput type={showPass ? "text" : "password"} label="Password" name="password" value={password} handleChange={handleDataChange} />
         {password && "filled" ? (
           <button type="button" className="login-showpass" onClick={() => setShowPass(!showPass)}>
             {showPass ? "Hide" : "Show"}
           </button>
         ) : null}
       </div>
-      <button
-        className="login-button"
-        type="submit"
-        disabled={!email || !password || password.length < 8}
-      >
-        Login
+      <button className="login-button" type="submit" disabled={!email || !password || password.length < 8}>
+        {alert.loading ? "Loading..." : "Login"}
       </button>
       {alert.error && <p className="alert">{alert.error}</p>}
       <p>
