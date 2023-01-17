cb738c52f6a28ae822dc18ae46815004b6b70f92
Tiago <tiagowu12@gmail.com>
2023-01-16 23:02:35 -0500

    changed the element of "/" path and used refreshToken

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,29 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Loading from "./pages/Loading";
+import { refreshToken } from "./redux/actions/authActions";
 
 function App() {
+  const { auth, alert } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshToken());
+  }, [dispatch]);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route
+          exact
+          path="/"
+          element={alert.loading ? <Loading /> : auth.token ? <Home /> : <Login />}
+        />
+        <Route exact path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
