13a011a50c189f08fb7c97330486b71d6539d050
Tiago <tiagowu12@gmail.com>
2023-01-16 22:53:56 -0500

    created fetchData using axios

---
+++ frontend/src/utils/fetchData.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+export const getData = async (url, token) => {
+  const res = await axios.get(`api/${url}`, {
+    headers: { Authorization: token },
+  });
+  return res;
+};
+
+export const postData = async (url, post, token) => {
+  const res = await axios.post(`api/${url}`, post, {
+    headers: { Authorization: token },
+  });
+
+  return res;
+};
+
+export const putData = async (url, post, token) => {
+  const res = await axios.put(`url/${url}`, post, {
+    headers: { Authorization: token },
+  });
+  return res;
+};
+
+export const patchData = async (url, post, token) => {
+  const res = await axios.patch(`url/${url}`, post, {
+    headers: { Authorization: token },
+  });
+  return res;
+};
+
+export const deleteData = async (url, post, token) => {
+  const res = await axios.delete(`url/${url}`, post, {
+    headers: { Authorization: token },
+  });
+  return res;
+};
