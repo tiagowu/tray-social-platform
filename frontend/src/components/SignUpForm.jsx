c1b70bc503b5ddaf19bc9bb3e58762eb4ff117f5
Tiago <tiagowu12@gmail.com>
2023-01-17 15:23:36 -0500

    combined user data into a single state and changed code formatting

--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -1,59 +1,45 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import TextInput from "./TextInput";
 import "./SignUpForm.css";
+import { signup } from "../redux/actions/authActions";
 
 const SignUpForm = () => {
-  const [email, setEmail] = useState("");
-  const [fullName, setFullName] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const { alert } = useSelector((state) => state);
+  const initialState = { email: "", fullName: "", username: "", password: "" };
   const [showPass, setShowPass] = useState(false);
+  const [userData, setUserData] = useState(initialState);
+  const { email, fullName, username, password } = userData;
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(signup(userData));
+  };
+
+  const handleDataChange = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
 
   return (
-    <form className="signup-form">
-      <TextInput
-        type="email"
-        label="Email"
-        value={email}
-        handleChange={(e) => {
-          setEmail(e.target.value);
-        }}
-      />
-      <TextInput
-        type="text"
-        label="Full Name"
-        value={fullName}
-        handleChange={(e) => {
-          setFullName(e.target.value);
-        }}
-      />
-      <TextInput
-        type="text"
-        label="Username"
-        value={username}
-        handleChange={(e) => {
-          setUsername(e.target.value);
-        }}
-      />
+    <form className="signup-form" onSubmit={handleSubmit}>
+      <TextInput type="text" label="Email" name="email" value={email} handleChange={handleDataChange} />
+      <TextInput type="text" label="Full Name" name="fullName" value={fullName} handleChange={handleDataChange} />
+      <TextInput type="text" label="Username" name="username" value={username.toLowerCase().replace(/ /g, "")} handleChange={handleDataChange} />
       <div className="password-input">
-        <TextInput
-          type={showPass ? "type" : "password"}
-          label="Password"
-          value={password}
-          handleChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
+        <TextInput type={showPass ? "type" : "password"} label="Password" value={password} name="password" handleChange={handleDataChange} />
         {password && "filled" ? (
           <button type="button" className="signup-showpass" onClick={() => setShowPass(!showPass)}>
             {showPass ? "Hide" : "Show"}
           </button>
         ) : null}
       </div>
-      <button className="signup-button" type="submit">
-        Sign Up
+      <button className="signup-button" type="submit" disabled={!email || !fullName || !username || !password}>
+        {alert.loading ? "Loading..." : "Sign Up"}
       </button>
+      {alert.error && <p className="alert">{alert.error}</p>}
       <p>
         Already have an account?{" "}
         <Link to="/" className="login-link">
