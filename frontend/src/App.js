6405ef38cf6f27a94e5baf3547f4970140090e36
Tiago <tiagowu12@gmail.com>
2023-01-19 23:11:10 -0500

    added PrivateRouter for profile pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import PrivateRouter from "./utils/PrivateRouter";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Profile from "./pages/Profile";
 import { refreshToken } from "./redux/actions/authActions";
 
 function App() {
@@ -21,6 +23,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={localStorage.getItem("login") || auth.token ? <Home /> : <Login />} />
         <Route exact path="/signup" element={localStorage.getItem("login") || auth.token ? <Navigate to="/" /> : <SignUp />} />
+        <PrivateRouter exact path="/profile/:id" element={<Profile />} />
       </Routes>
     </Router>
   );
