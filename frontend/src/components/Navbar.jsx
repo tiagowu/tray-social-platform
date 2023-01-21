bc9dec27cf606cadc79e53faadebbbedd189554a
Tiago <tiagowu12@gmail.com>
2023-01-20 22:18:46 -0500

    cleaned up code and removed inline css styles

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { logout } from "../redux/actions/authActions";
+
 import { AppBar, IconButton, Toolbar, Typography, Stack } from "@mui/material";
+import { StyledEngineProvider } from "@mui/material/styles";
 import RoomServiceOutlinedIcon from "@mui/icons-material/RoomServiceOutlined";
 import LogoutIcon from "@mui/icons-material/Logout";
 import HomeIcon from "@mui/icons-material/Home";
 import Avatar from "@mui/material/Avatar";
+
+import SearchBar from "./SearchBar";
+import { logout } from "../redux/actions/authActions";
+import "./Navbar.css";
 
 const Navbar = () => {
   const { auth } = useSelector((state) => state);
@@ -15,49 +20,39 @@ const Navbar = () => {
 
   const handleLogout = () => {
     dispatch(logout());
-  };
-  const customizeToolbar = {
-    minHeight: 50,
-  };
-
-  const linkStyle = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    textDecoration: "none",
-    color: "black",
+    window.location.reload();
   };
 
   return (
-    <AppBar sx={{ background: "#6f81a5" }}>
-      <Toolbar style={customizeToolbar} sx={{ justifyContent: "space-between", minHeight: 50 }}>
-        <IconButton
-          disableRipple
-          onClick={() => navigate("/")}
-          sx={{ color: "white", borderRadius: 0, "&:hover": { backgroundColor: "transparent" } }}
-        >
-          <RoomServiceOutlinedIcon sx={{ fontSize: 40, color: "white" }} />
-          <Typography variant="h4" sx={{ ml: 1 }}>
-            Tray
-          </Typography>
-        </IconButton>
-
-        <Stack direction="row" component="div" spacing={0}>
-          <Link to={`/profile/${auth.user._id}`} style={linkStyle}>
-            <Avatar />
-            <Typography variant="h7" margin="0px 12px 0px 8px">
-              {auth.user.fullname}
-            </Typography>
-          </Link>
-          <IconButton sx={{ bgcolor: "#5b6d8f", "&:hover": { bgcolor: "#4b5c7d" } }}>
-            <HomeIcon sx={{ fontSize: 30, color: "white" }} />
-          </IconButton>
-          <IconButton sx={{ bgcolor: "#5b6d8f", "&:hover": { bgcolor: "#4b5c7d" } }} onClick={handleLogout}>
-            <LogoutIcon sx={{ fontSize: 30, color: "white" }} />
-          </IconButton>
-        </Stack>
-      </Toolbar>
-    </AppBar>
+    <StyledEngineProvider injectFirst>
+      <AppBar className="appBar">
+        <Toolbar className="toolBar">
+          <Stack direction="row">
+            <IconButton className="logo" disableRipple onClick={() => navigate("/")}>
+              <RoomServiceOutlinedIcon className="buttonIcon" />
+              <Typography className="logoName" variant="h4">
+                Tray
+              </Typography>
+            </IconButton>
+            <SearchBar />
+          </Stack>
+          <Stack direction="row">
+            <Link className="profileLink" to={`/profile/${auth.user._id}`}>
+              <Avatar />
+              <Typography className="profileName" variant="h7">
+                {auth.user.fullname}
+              </Typography>
+            </Link>
+            <IconButton className="button">
+              <HomeIcon className="buttonIcon" />
+            </IconButton>
+            <IconButton className="button" onClick={handleLogout}>
+              <LogoutIcon className="buttonIcon" />
+            </IconButton>
+          </Stack>
+        </Toolbar>
+      </AppBar>
+    </StyledEngineProvider>
   );
 };
 
