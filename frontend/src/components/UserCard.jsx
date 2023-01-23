e609bab57ced63eb15345c699bec6aebab3be36d
Tiago <tiagowu12@gmail.com>
2023-01-22 21:52:37 -0500

    created UserCard for SearchBar component and added styles to it

---
+++ frontend/src/components/UserCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { Avatar } from "@mui/material";
+import "./UserCard.css";
+
+const UserCard = ({ user }) => {
+  return (
+    <Link className="user-card" to={`profile/${user._id}`} key={user._id}>
+      <Avatar className="card-avatar" src={user.avatar} />
+      <div className="card-info">
+        <span className="card-username">{user.username}</span>
+        <span className="card-fullname">{user.fullName}</span>
+      </div>
+    </Link>
+  );
+};
+
+export default UserCard;
