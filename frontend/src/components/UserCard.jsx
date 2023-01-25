f6f7ecce5c52bf1fd7aa9934998ba9154b7a8e1c
Tiago <tiagowu12@gmail.com>
2023-01-24 22:28:33 -0500

    fixed to link to user profiles

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -6,7 +6,7 @@ import "./UserCard.css";
 
 const UserCard = ({ user }) => {
   return (
-    <Link className="user-card" to={`profile/${user._id}`} key={user._id}>
+    <Link className="user-card" to={`/profile/${user._id}`} key={user._id}>
       <Avatar className="card-avatar" src={user.avatar} />
       <div className="card-info">
         <span className="card-username">{user.username}</span>
