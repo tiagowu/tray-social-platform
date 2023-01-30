a5b40cc448d0e33c5143e88e7ccade8e95dc2e4d
Tiago <tiagowu12@gmail.com>
2023-01-29 21:13:18 -0500

    removed the 's' in users

--- backend/controllers/userCtrl.js
+++ backend/controllers/userCtrl.js
@@ -16,7 +16,7 @@ const userCtrl = {
     try {
       const user = await Users.findOne({ _id: req.params.id }).select("-password");
       if (!user) return res.status(400).json({ msg: "User does not exist." });
-      res.json({ users });
+      res.json({ user });
     } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
