7aa7b0dc25d5bda274758bf27984d0d349db8015
Tiago <tiagowu12@gmail.com>
2023-01-30 22:44:47 -0500

    added get user profile function

--- frontend/src/redux/actions/profileActions.js
+++ frontend/src/redux/actions/profileActions.js
@@ -1,4 +1,36 @@
+import { getData } from "../../utils/fetchData";
+
 export const PROFILE_TYPES = {
   LOADING: "LOADING",
   GET_USER: "GET_USER",
 };
+
+export const getUserProfile =
+  ({ users, id, auth }) =>
+  async (dispatch) => {
+    if (users.every((user) => user._id !== id)) {
+      try {
+        dispatch({
+          type: PROFILE_TYPES.LOADING,
+          payload: { loading: true },
+        });
+        const res = await getData(`user/${id}`, auth.token);
+        console.log(res);
+        dispatch({
+          type: PROFILE_TYPES.GET_USER,
+          payload: res.data,
+        });
+        dispatch({
+          type: PROFILE_TYPES.LOADING,
+          payload: { loading: false },
+        });
+      } catch (error) {
+        dispatch({
+          type: "ALERT",
+          payload: {
+            error: error.response.data.msg,
+          },
+        });
+      }
+    }
+  };
