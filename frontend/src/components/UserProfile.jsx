3cc91de97b1d72c4fb83f46df2a32536e8fe23a2
Tiago <tiagowu12@gmail.com>
2023-01-31 22:33:23 -0500

    used get user profile function to obtain user data

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -3,22 +3,25 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
+import { getUserProfile } from "../redux/actions/profileActions";
+
 const UserProfile = () => {
   const { id } = useParams();
-  const { auth } = useSelector((state) => state);
+  const { auth, profile } = useSelector((state) => state);
   const [userData, setUserData] = useState([]);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (auth.user && auth.user._id === id) {
+    if (auth && auth.user && auth.user._id === id) {
       setUserData([auth.user]);
+    } else {
+      dispatch(getUserProfile({ users: profile.users, id, auth }));
     }
-  }, [auth.user, id]);
+  }, [auth.user, id, auth]);
 
-  console.log(userData);
   return (
     <div>
-      {userData.length > 0 && userData.map((user) => <div>{user.fullName}</div>)}
+      {userData.length > 0 && userData.map((user) => <div key={user._id}>{user.fullName}</div>)}
       <h1>Info - {id}</h1>
     </div>
   );
