528860336b602e4a9f6c8c5fff30299a9e882660
Tiago <tiagowu12@gmail.com>
2023-02-06 22:47:42 -0500

    reorganized the divs to better apply flexbox

--- frontend/src/components/ProfileInfo.jsx
+++ frontend/src/components/ProfileInfo.jsx
@@ -10,23 +10,23 @@ const ProfileInfo = ({ user }) => {
         <div className="profileinfo-top">
           <img src="https://wallpaperaccess.com/full/1493766.jpg" />
         </div>
-        <div className="profileinfo-bottom">
+        <div className="profileinfo-bottomcontainer">
           <Avatar className="profileinfo-avatar" src={user.avatar} />
-
-          <div className="profileinfo-bottomcenter">
-            <div className="profileinfo-names">
-              <Typography variant="h5">Usernameeeeeeeeeeeeeeee</Typography>
-              <Typography variant="h6">Fullname</Typography>
+          <div className="profileinfo-bottom">
+            <div className="profileinfo-bottomtop">
+              <div className="profileinfo-names">
+                <Typography variant="h5">Fullnameeeeeeeeeeeee</Typography>
+                <Typography variant="h6">Usernameeeeeeeeeeeeeeee</Typography>
+              </div>
+              <div className="profileinfo-follow">
+                <Button className="follow-button">Follow</Button>
+              </div>
             </div>
-            <div className="profileinfo-follow">
-              <Button className="follow-button">Follow</Button>
+            <div className="profileinfo-stats">
+              <Typography variant="h6">0 Posts</Typography>
+              <Typography variant="h6">0 Followings</Typography>
+              <Typography variant="h6">0 Folowers</Typography>
             </div>
-          </div>
-
-          <div className="profileinfo-stats">
-            <Typography variant="h6">0 Posts</Typography>
-            <Typography variant="h6">0 Followings</Typography>
-            <Typography variant="h6">0 Folowers</Typography>
           </div>
         </div>
       </div>
