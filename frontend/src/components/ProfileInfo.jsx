fc27ab917a6093f1de813988024bc44422518e05
Tiago <tiagowu12@gmail.com>
2023-02-08 22:46:48 -0500

    changed profile info to include user data and made edit profile button

--- frontend/src/components/ProfileInfo.jsx
+++ frontend/src/components/ProfileInfo.jsx
@@ -3,29 +3,42 @@ import { Avatar, Button, StyledEngineProvider, Typography } from "@mui/material"
 
 import "./ProfileInfo.css";
 
-const ProfileInfo = ({ user }) => {
+const ProfileInfo = ({ auth, id, user, profile }) => {
   return (
     <StyledEngineProvider injectFirst>
       <div className="profileinfo-container">
         <div className="profileinfo-top">
-          <img src="https://wallpaperaccess.com/full/1493766.jpg" />
+          <img src="https://wallpaperaccess.com/full/1493766.jpg" alt="" />
         </div>
         <div className="profileinfo-bottomcontainer">
           <Avatar className="profileinfo-avatar" src={user.avatar} />
           <div className="profileinfo-bottom">
             <div className="profileinfo-bottomtop">
               <div className="profileinfo-names">
-                <Typography variant="h5">Fullnameeeeeeeeeeeee</Typography>
-                <Typography variant="h6">Usernameeeeeeeeeeeeeeee</Typography>
+                <Typography className="profileinfo-name" variant="h4">
+                  {user.fullName}
+                </Typography>
+                <Typography variant="h6">@{user.username}</Typography>
               </div>
-              <div className="profileinfo-follow">
-                <Button className="follow-button">Follow</Button>
+
+              <div className="profileinfo-button">
+                {auth.user._id !== id ? (
+                  <Button className="follow-button">Follow</Button>
+                ) : (
+                  <Button className="editprofile-button">Edit Profile</Button>
+                )}
               </div>
             </div>
             <div className="profileinfo-stats">
-              <Typography variant="h6">0 Posts</Typography>
-              <Typography variant="h6">0 Followings</Typography>
-              <Typography variant="h6">0 Folowers</Typography>
+              <Typography className="profileinfo-stat" variant="h6">
+                {profile.posts.length} Posts
+              </Typography>
+              <Typography className="profileinfo-stat" variant="h6">
+                {user.followings.length} Followings
+              </Typography>
+              <Typography className="profileinfo-stat" variant="h6">
+                {user.friends.length} Folowers
+              </Typography>
             </div>
           </div>
         </div>
